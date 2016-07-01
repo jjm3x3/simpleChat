9bc2d5b83f8dcd3e9db65c6239c93b8b36d4cdd1
Jacob M <jmeixner@uwm.edu>
2016-06-30 19:08:16 -0700

    Reeeeeeal simple enctyption

--- simpleClient.js
+++ simpleClient.js
@@ -33,6 +33,58 @@ rl.setPrompt('OHHI> ');
 rl.prompt();
 
 rl.on('line', (line) => {
-	client.write(line);
+  console.log(line);
+  var decmalString = "";
+  for(i = 0; i< line.length; ++i ){
+    decmalString += line.charCodeAt(i).toString(16);
+  }
+  var message = parseInt("0x" + decmalString);
+  console.log(message);
+
+  p = 2053
+  q = 7919
+  n = p*q
+  t = (p-1) * (q -1);
+  // set e = relativly prime number to t
+  // invoves doing prime factorization
+  e = 17  // this makes our d wrong is in tearms of 17
+  // figue out a way to come up with d
+  d = 10513241
+
+  c = binSqr(e,n,message);
+  console.log("here is the cryptic message: " + c);
+  console.log("uncryptoed message: " + binSqr(d,n,c));
+  var cryptoMessage = message ^ e;
+
+	client.write(cryptoMessage.toString());
 	rl.prompt();
 });
+
+function binSqr(e,n,m){
+   var result = m; // might not always be mod n
+   while(e>1){    
+    k = m;
+    s = e.toString(2);
+    // console.log("s.length: " + s.length);
+   
+    for(i = s.length; i>1; i--){
+      k = Math.pow(k,2);
+      k = k%n;
+    }
+    someNum = Math.pow(2, (s.length-1));
+    // console.log("removed portion: " + someNum);
+    e = e-someNum;
+
+    console.log("here is k: " + k);
+    result = result * k;
+    result = result % n;
+
+    debugger;
+    // console.log("s: " + s);
+    // console.log("e: " + e);
+
+  }
+  return result;
+}
+
+
---
+++ toy.bat
@@ -0,0 +1,3 @@
+start node simpleServer.js
+start node simpleClient.js localhost
+node simpleClient.js localhost
